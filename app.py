67879b0acd73f6098c7ce56767d7cb6c3a549441
Your Name <ueay@pm.me>
2022-05-30 16:46:48 +0200

    add eda app

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import webscraper # import your app modules here
+from apps import webscraper, eda # import your app modules here
 
 app = MultiApp()
 
-st.markdown("""
-# Multi-Page App
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-""")
-
 # Add all your application here
 app.add_app("1. Data Acquisition", webscraper.app)
-#app.add_app("Data", data.app)
+app.add_app("2. Exploratory Data Analysis", eda.app)
 #app.add_app("Model", model.app)
 # The main app
 app.run()
+
+st.markdown("""
+This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). 
+""")
---
+++ apps/eda.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def app():
+    st.title("2. Exploratory Data Analysis")
